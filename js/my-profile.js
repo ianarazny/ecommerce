759b448062d3fb79fc02da6faea30c077df1e6b2
ianarazny <ian.arazny.c@gmail.com>
2021-10-21 10:57:47 -0300

    Perfil Completo

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,54 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+"use strict";
+
+var array = [];
+function actualizar() {
+    const user = document.getElementById('changeNameOfUser').value;
+    const surnameUser = document.getElementById('changeSurnameOfUser').value;
+    const emailUser = document.getElementById('changeInputEmail').value;
+    const numberOfUser = document.getElementById('changeTelefono').value;
+
+    console.log(user, surnameUser, emailUser, numberOfUser);
+    array.push({
+        user,
+        surnameUser,
+        emailUser,
+        numberOfUser
+    });
+
+    localStorage.setItem('perfil', JSON.stringify(array));
+
+};
+function actualizarCard() {
+    if (localStorage.getItem('perfil')) {
+        let infoUsuario = JSON.parse(localStorage.getItem('perfil'));
+
+        var nombre = document.getElementById('nameOfUser');
+        nombre.innerHTML += infoUsuario[0].user;
+
+        var apellido = document.getElementById('surnameOfUser');
+        apellido.innerHTML += infoUsuario[0].surnameUser;
+
+        var mail = document.getElementById('inputEmail');
+        mail.innerHTML += infoUsuario[0].emailUser;
+
+        var numero = document.getElementById('telefono');
+        numero.innerHTML += infoUsuario[0].numberOfUser;
+
+
+
+        console.log(JSON.parse(localStorage.getItem('perfil')));
+        console.log(infoUsuario[0].user);
+        console.log(infoUsuario[0].numberOfUser)
+
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById('actualizarInfo').addEventListener('click', actualizar);
+    actualizarCard();
 });
