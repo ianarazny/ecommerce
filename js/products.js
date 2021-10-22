ad5e42c691c7b7bc777a3f7ebd9f754f8acfa6a5
ianarazny <ian.arazny.c@gmail.com>
2021-10-21 21:34:58 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -61,23 +61,6 @@ function showProductsList() {
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
     }
 }
-{/* <a href="product-info.html" class="list-group-item list-group-item-action">
-<div class="row">
-    <div class="col-3">
-        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
-    </div>
-    <div class="col">
-        <div class="d-flex w-100 justify-content-between">
-            <h4 class="mb-1">`+ products.name + `</h4>
-            <small class="text-muted">` + products.soldCount + ` artículos vendidos</small>
-        </div>
-        <p class="mb-1">` + `Descripción: ` + products.description + `</p>
-        <p class="mb-1">` + `Costo: ` + products.cost + `</p>
-        <p class="mb-1">` + `Moneda: ` + products.currency + `</p>
-    </div>
-</div>
-</a> */}
-
 function sortAndShowProducts(sortCriteria, prodArray) {
     currentSortCriteria = sortCriteria;
 
